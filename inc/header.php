097c9e1b0a691239aadef0799450102f60e88a10
balanasia <anabalasia@gmail.com>
2022-12-13 15:10:36 -0600

    Create a Header and Footer Template and include them into the pages

--- inc/header.php
+++ inc/header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <title>Leave Feedback</title>
+  <title>Anastasiia Balakhan</title>
 </head>
 <body>
   <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
@@ -17,13 +17,13 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
          <li class="nav-item">
-           <a class="nav-link" href="/php-crash/feedback/index.html">Home</a>
+           <a class="nav-link" href="/feedback-app/index.php">Home</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link" href="/php-crash/feedback/feedback.html">Feedback</a>
+           <a class="nav-link" href="/feedback-app/feedback.php">Feedback</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link" href="/php-crash/feedback/about.html">About</a>
+           <a class="nav-link" href="/feedback-app/about.php">About</a>
          </li>
         </ul>
       </div>
@@ -32,17 +32,3 @@
 
 <main>
   <div class="container d-flex flex-column align-items-center">
-    <h2>About</h2>
-
-    <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?</p>
-    </div>
-</main>
-
-<footer class="text-center mt-5">
-  Copyright &copy; 2022
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
